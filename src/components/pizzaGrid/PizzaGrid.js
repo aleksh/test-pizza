2d063542f69ac47339f73718fe12f9e8547dd774
oleksii-hostaway <oleksii@hostaways.com>
2020-04-26 18:47:20 +0300

    setup firebase

--- src/components/pizzaGrid/PizzaGrid.js
+++ src/components/pizzaGrid/PizzaGrid.js
@@ -13,7 +13,9 @@ function PizzaGrid() {
 	const dispatch = useContext(GlobalDispatchContext);
 
 	useEffect(() => {
-		dispatch({ type: "SET_PIZZAS", payload: PizzasConst });
+    if(pizzas.length === 0) {
+      dispatch({ type: "SET_PIZZAS", payload: PizzasConst });
+    }
 	}, []);
 
 	const addToBasket = (pizza) => {
@@ -49,8 +51,8 @@ const PizzasConst = [
 		imageUrl:
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
-			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
+			"https://mafia.ua/storage/editor/fotos/100x100/mafiya.jpeg",
+		price: 100,
 		title: "Mafia",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -58,33 +60,33 @@ const PizzasConst = [
 	{
 		id: "2",
 		imageUrl:
-			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
+			"https://mafia.ua/storage/editor/fotos/480x0/picca-ohotnichya_1542267892836.jpeg",
 		icon:
-			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+			"https://mafia.ua/storage/editor/fotos/100x100/picca-ohotnichya.jpeg",
+		price: 119,
+		title: "Mafia 111",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
 	{
 		id: "3",
 		imageUrl:
-			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
+			"https://mafia.ua/storage/editor/fotos/480x0/margarita_156706066239.jpeg",
 		icon:
-			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+			"https://mafia.ua/storage/editor/fotos/100x100/margarita_1567060656531.jpeg",
+		price: 89,
+		title: "Mafia 333",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
 	{
 		id: "4",
 		imageUrl:
-			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
+			"https://mafia.ua/storage/editor/fotos/480x0/karbonara_1567060589165.jpeg",
 		icon:
-			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+			"https://mafia.ua/storage/editor/fotos/100x100/karbonara_1567060586370.jpeg",
+		price: 200,
+		title: "Mafia 44",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -95,7 +97,7 @@ const PizzasConst = [
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
 		price: 189,
-		title: "Mafia",
+		title: "Mafia 555",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -105,8 +107,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 289,
+		title: "Mafia 66",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -116,8 +118,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 19,
+		title: "Mafia 77",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -127,8 +129,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 289,
+		title: "Mafia 99",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -138,8 +140,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 90,
+		title: "Mafia 99",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -149,8 +151,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 89,
+		title: "Mafia 10",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -160,8 +162,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 29,
+		title: "Mafia 111",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -171,8 +173,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 83,
+		title: "Mafia 121212",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
@@ -182,8 +184,8 @@ const PizzasConst = [
 			"https://mafia.ua/storage/editor/fotos/480x0/mafiya_1581069452273.jpeg",
 		icon:
 			"https://mafia.ua/storage/editor/fotos/100x100/amerikano_156706005058.jpeg",
-		price: 189,
-		title: "Mafia",
+		price: 200,
+		title: "Mafia 131313",
 		description:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 	},
